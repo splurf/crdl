0def0c65e4925265ee3ec230999c0645781ae8fc
splurf <evandotschwartzentruber@gmail.com>
2023-12-07 01:20:33 -0500

    add additional flags for yt-dlp

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod cfg;
 mod err;
+mod util;
 
-use {cfg::*, err::Result};
+use {cfg::*, err::Result, util::*};
 
 fn main() -> Result<()> {
     let cfg = Config::new();
 
     if let Some(url) = cfg.url() {
-        download(url)
+        download(url, cfg.other_flags())
     } else {
         requirements()
     }
